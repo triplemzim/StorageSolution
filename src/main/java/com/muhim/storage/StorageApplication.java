494e3dd12115117db5a827b24691dfbed8fd189f
Muhim <triplemzim@gmail.com>
2024-06-29 12:34:25 +0800

    Spring initialized - first commit

---
+++ src/main/java/com/muhim/storage/StorageApplication.java
@@ -0,0 +1,18 @@
+package com.muhim.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+public class StorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageApplication.class, args);
+	}
+
+}
